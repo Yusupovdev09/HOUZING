1a3af83bdc5966ba331c6fc9353149a6ec716e04
YusupovDev09 <yusupovabdulaziz222@gmail.com>
2022-12-20 17:30:41 +0300

    filter

---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Input } from '../Generic';
+import { Container } from './style'
+
+export const Home = () => {
+    return (
+        <Container>
+            <h1>Home</h1>
+            <Input width={200} placeholder={"test"} />
+        </Container>
+    )
+}
+export default Home;
--- src/components/Home/Generic/Button/index.jsx
+++ src/components/Home/Generic/Button/index.jsx
@@ -3,9 +3,9 @@ import { Container } from './style'
 
 export const Button = ({ children, type, onClick, width, height }) => {
     return (
-        <Container onClick={onClick} width={width} height={height} type={type} >
+        <Container onClick={onClick} width={width} height={height } type={type} >
             {children || "Generic Button"}
         </Container>
     )
 }
-export default Button;
+export default Button; 
